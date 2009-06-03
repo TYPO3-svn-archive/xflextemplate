226ef002a4f6a002720547812b1fb412c3ef2e00
federico@bernardin.it <federico@bernardin.it@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-06-03 20:17:59 +0000

    Initial import
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/xflextemplate/tags/beta-2.0.6@21019 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ configuration/elementConfiguration.php
@@ -0,0 +1,15 @@
+<?php
+$GLOBALS['configuration'] = array(
+	'subElement' => array(
+		'main' => array('titleLabel', 'typeLabel', 'xtypeLabel', 'paletteLabel'),
+		'type' => array('inputType', 'textType', 'radioType', 'checkType', 'groupType', 'cObjectType'),
+		'xtype' => array('noneXtype', 'textXtype', 'imageXtype', 'multimediaXtype', 'cObjectXtype'),
+		'inputType' => array('max', 'size', 'default', 'is_in', 'eval', 'checkbox', 'maxval', 'minval'),
+		'textType' => array('cols', 'rows', 'default', 'wrap', 'defaultExtras'),
+		'checkType' => array('items','cols', 'default', 'itemprocfunc'),
+		'radioType' => array('items', 'default', 'itemprocfunc'),
+		'groupType' => array('internal_type', 'allowed', 'disallowed', 'MM', 'max_size', 'show_thumbs', 'maxitems', 'minitems', 'size', 'autoSizeMax', 'multiple'),
+		'cObjectType' => array(),
+	),
+);
+?>
