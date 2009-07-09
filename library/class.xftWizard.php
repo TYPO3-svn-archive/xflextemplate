6981a41d56e088742dea109fd860ec776e280edc
federico@bernardin.it <federico@bernardin.it@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-07-09 20:38:30 +0000

    reset trunk version to 2.0.7
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/xflextemplate/trunk@22202 735d13b6-9817-0410-8766-e36946ffe9aa

--- library/class.xftWizard.php
+++
@@ -1,8 +0,0 @@
-<?php
-    abstract class xftWizard{
-    	abstract public function render($compactField);
-		
-		abstract public function getCompactField($postArray);
-		abstract public function renderPageElement($content,$conf);
-    }
-?>
