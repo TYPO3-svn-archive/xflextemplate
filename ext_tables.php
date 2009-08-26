233633060c32430d15ec0e333dfb7bdfceb668ae
federico@bernardin.it <federico@bernardin.it@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-08-26 09:30:31 +0000

    Add start and stop field in Backend Add new feature in extension configuration Add Export and import feature
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/xflextemplate/trunk@23667 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -2,14 +2,14 @@
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
 $tempColumns = Array (
-	"xtemplate" => Array (
-		"exclude" => 1,
-		"label" => "LLL:EXT:xflextemplate/locallang_db.xml:tt_content.xtemplate",
-		"config" => Array (
-			"type" => "select",
-			"itemsProcFunc"=>"tx_xflextemplate_handletemplate->main",
-		)
-	),
+  "xtemplate" => Array (
+    "exclude" => 1,
+    "label" => "LLL:EXT:xflextemplate/locallang_db.xml:tt_content.xtemplate",
+    "config" => Array (
+      "type" => "select",
+      "itemsProcFunc"=>"tx_xflextemplate_handletemplate->main",
+    )
+  ),
 
 );
 
@@ -20,7 +20,7 @@ t3lib_extMgm::addTCAcolumns("tt_content",$tempColumns,1);
 
 
 t3lib_div::loadTCA('tt_content');
-$TCA['tt_content']['types'][$_EXTKEY.'_pi1']['showitem']='CType;;4;;1-1-1,hidden,header;LLL:EXT:lang/locallang_general.php:LGL.name;;;2-2-2,xtemplate;;4;;1-1-1'.$strType;
+$TCA['tt_content']['types'][$_EXTKEY.'_pi1']['showitem']='CType;;14;;1-1-1,hidden;;1;;1-1-1,header;LLL:EXT:lang/locallang_general.php:LGL.name;;;2-2-2,xtemplate,--div--;LLL:EXT:xflextemplate/locallang_db.xml:xftDiv';
 $TCA['tt_content']['ctrl']['requestUpdate']='xtemplate';
 
 t3lib_extMgm::addStaticFile($_EXTKEY,"pi1/static/","XFlexTemplate");
@@ -31,7 +31,7 @@ t3lib_extMgm::addPlugin(Array('LLL:EXT:xflextemplate/locallang_db.xml:tt_content
 
 //Is set the constant into ExtManager xflextemplate will be the default content element in place of text
 if ($xflextemplateConfiguration['xflextemplateIsStandardContentElement']){
-	$TCA['tt_content']['columns']['CType']['config']['default'] = 'xflextemplate_pi1';
+  $TCA['tt_content']['columns']['CType']['config']['default'] = 'xflextemplate_pi1';
 }
 
 include_once(t3lib_extMgm::extPath('xflextemplate').'library/class.tx_xflextemplate_handletemplate.php');
@@ -39,9 +39,9 @@ include_once(t3lib_extMgm::extPath('xflextemplate').'hooks/class.tx_xflextemplat
 include_once(t3lib_extMgm::extPath('xflextemplate').'hooks/class.tx_xflextemplate_tcemain.php');
 
 if (TYPO3_MODE=='BE')	{
-	t3lib_extMgm::addModule('tools','txxflextemplateM1','',t3lib_extMgm::extPath($_EXTKEY).'mod1/');
-	//include wizard for displaying xft as common element
-	$TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["tx_xflextemplate_pi1_wizicon"] = t3lib_extMgm::extPath($_EXTKEY)."pi1/class.tx_xflextemplate_pi1_wizicon.php";
+  t3lib_extMgm::addModule('tools','txxflextemplateM1','',t3lib_extMgm::extPath($_EXTKEY).'mod1/');
+  //include wizard for displaying xft as common element
+  $TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["tx_xflextemplate_pi1_wizicon"] = t3lib_extMgm::extPath($_EXTKEY)."pi1/class.tx_xflextemplate_pi1_wizicon.php";
 }
 
 
